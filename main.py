4e9fdbdfecc9190b5f89905365cba6b2f8567994
arunsanker <73099179+arunsanker@users.noreply.github.com>
2021-03-26 19:55:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ from config import API_ID, API_HASH, BOT_TOKEN
 
 bot = Bot(
     ":memory:",
-    API_ID,
-    API_HASH,
-    bot_token=BOT_TOKEN,
+    API_ID,3855175
+    API_HASH,c7d4b8b7c1259bb0b7fd836b99eb657b
+    bot_token=BOT_TOKEN,1763607864:AAFKvaZBzxn5t5_VYBtI8ytZh9m6Cg4Kopo
     plugins=dict(root="handlers")
 )
 
